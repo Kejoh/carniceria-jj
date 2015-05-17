f318a2134934bbcc8cd2d21287e6ca62de13a12b
Kejoh <kejoh12@gmail.com>
2015-05-18 00:28:57 +0100

    hacer carousel

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,13 +2,6 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
 .controller('ImágenestCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
